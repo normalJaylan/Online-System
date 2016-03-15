1dd8d987c4f7daf8a9b1e07578c6bf5fe471e80c
normalJaylan <houxinglin@gmail.com>
2016-03-15 12:30:15 +0800

    初始化了留言功能

---
+++ System/src/com/project/WordInfo.java
@@ -0,0 +1,32 @@
+package com.project;
+
+public class WordInfo {
+	private String patient;
+	private String doctor;
+	private String context;
+	
+	public String getPatient() {
+		return patient;
+	}
+	
+	public void setPatient(String patient) {
+		this.patient = patient;
+	}
+	
+	public String getDoctor() {
+		return doctor;
+	}
+	
+	public void setDoctor(String doctor) {
+		this.doctor = doctor;
+	}
+	
+	public String getContext() {
+		return context;
+	}
+	
+	public void setContext(String context) {
+		this.context = context;
+	}
+	
+}
